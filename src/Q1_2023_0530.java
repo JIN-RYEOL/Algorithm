c0366b8dbc5fcaabd978e29419b2deeac92f9291
JIN-RYEOL <0205kjl@naver.com>
2023-05-30 23:32:37 +0900

    Q1_2023_0530 약수의 갯수와 덧셈

--- src/Q1_2023_0530.java
+++ src/Q1_2023_0530.java
@@ -39,19 +39,18 @@ public class Q1_2023_0530 {
         int answer = 0;
         int count = 0;
 
-
         for (int i = 13; i <= 17; i++) {
             for (int j = 1; j <= i; j++) {
                 if (i % j == 0) {
                     count++;
                 }
-            } // 안쪽 for
+            }
             if (count % 2 == 0) {
                 answer += i;
             } else {
                 answer -= i;
             }
-        } // 바깥쪽 for
+        }
         return;
     }
 }
